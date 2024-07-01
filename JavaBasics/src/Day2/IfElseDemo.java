7f9d9353e3d179ec82dc3f5b6a97c9542390110c
Jananieshwari <jananieshwaripalani@gmail.com>
2024-07-01 19:57:49 +0530

    this is my day2 programs

---
+++ JavaBasics/src/Day2/IfElseDemo.java
@@ -0,0 +1,15 @@
+package Day2;
+
+public class IfElseDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int age=11;
+    if(age>=18)
+    	System.out.println("You ara eligible for Voting");
+    else
+    	System.out.println("Sorry!you need to wait more");
+        System.out.println("if else demonstration");
+	}
+
+}
