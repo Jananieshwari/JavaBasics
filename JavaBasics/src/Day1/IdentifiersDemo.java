2e76f27d2c98aebdd4f6467afda4e03c426f1915
Jananieshwari <jananieshwaripalani@gmail.com>
2024-07-01 11:16:01 +0530

    this is a Day 1 program

---
+++ JavaBasics/src/Day1/IdentifiersDemo.java
@@ -0,0 +1,15 @@
+package Day1;
+
+public class IdentifiersDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int number2=13;
+    System.out.println("Value of the number variable is:"+number2);
+    int $number=20;
+    System.out.println("Value of the number variables is :"+$number);
+    String studentName="Aniket";
+    System.out.println("value is :"+studentName);
+	}
+
+}
---
+++ JavaBasics/src/Day1/PrimitiveDataTypeDemo.java
@@ -0,0 +1,26 @@
+package Day1;
+
+public class PrimitiveDataTypeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    byte byteMax=127;
+    byte byteMin=-128;
+    System.out.println("Min range of byte is"+byteMin+"Max range of byte is"+byteMax);
+    short shortMax=32767;
+    short shortMin=-32768;
+    System.out.println("Minshort of byte is"+shortMin +"Maxshort range of byte is"+shortMax);
+    int maxInt=2147483647;
+    int minInt=-147483648;
+    System.out.println("minInt range of byte is"+minInt+"Maxint range of byte is"+maxInt);
+    long maxLong=9223372036854775807L;
+    long minLong=-9223372036854775808L;
+    System.out.println("Minlong range of byte is"+minLong+"MaxLong range of byte is"+maxLong);
+    float f=2324.432467234862383f;
+    double d=2343.3324234264872825982864f;
+    System.out.println("float value is "+f+"double value is"+d);
+    boolean flag=false;
+    System.out.println("boolean value is"+flag);
+	}
+
+}
